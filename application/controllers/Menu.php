f541b8c20f1b8f7e749c4663974f52dd3424afdd
sarahsitis <sarahsitis@gmail.com>
2019-12-09 18:42:12 +0700

    add user access feature. Create booku_helper.php to add user limitation access. Create blocked.php which will display if someone that isn't an admin access some restricted pages

--- application/controllers/Menu.php
+++ application/controllers/Menu.php
@@ -6,6 +6,7 @@ class Menu extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+        is_logged_in();
         $this->load->model('Menu_model', 'menu');
     }
     public function index()
